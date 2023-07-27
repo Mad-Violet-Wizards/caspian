880019dc6eaca96b21d4f927f721a033b4943758
Mateusz <316072@uwr.edu.pl>
2023-07-27 22:46:12 +0200

    Extended ImGuiTools.
    * Vendor: Added imgui-file-dialog library, dirent and stb libraries.
    * Tools/ImGui: ImGuiToolbar has access to ImGuiManager via extra field.
    * Tool/ImGui: Completed ImGuiToolbar::Render method.
    * Tools/ImGui: Added prototype ImGuiImportAssetWindow.
    * Tools/ImGui: Added constexpr init active state for imgui.
    * Tools/ImGui: Extra utils namespace with flags & styles helper structs.
    * Tools/ImGui: Fixed access modifiers for IImGuiWidget.

--- engine/Tools/ImGuiTools.cpp
+++ engine/Tools/ImGuiTools.cpp
@@ -1,33 +1,163 @@
 #include "engine/pch.hpp"
 
-#include <imgui/imgui.h>
+#include <imgui-file-dialog/ImGuiFileDialog.h>
+#include <iostream>
+
 #include "engine/Tools/ImGuiTools.hpp"
 
 using namespace Tools;
 
 ////////////////////////////////////////////////////////
-/* ImGuiMenubar */
-void ImGuiMenubar::Update(float _dt)
+/* ImGuiToolbar */
+ImGuiToolbar::ImGuiToolbar(ImGuiManager* _mgr)
+	: m_Manager(_mgr)
+{
+}
+
+void ImGuiToolbar::Update(float _dt)
+{
+	if (!m_Active)
+		return;
+}
+
+void ImGuiToolbar::Render()
+{
+	if (!m_Active)
+		return;
+
+	static auto flags = utils::flags();
+
+	ImGui::Begin("Toolbar", &m_Active, flags.toolbar_window_flags);
+	{
+		ImGui::SetWindowPos(ImVec2(0, 0));
+		ImGui::SetWindowSize(ImVec2(ImGui::GetIO().DisplaySize.x, 60));
+
+		static auto styles = utils::styles();
+
+		styles.toolbar_push_combobox_style();
+		styles.toolbar_push_button_style();
+
+		{
+			constexpr auto assets_popup_name = "AssetsPopup";
+
+			if (ImGui::Button("Assets", styles.toolbar_button_size))
+				ImGui::OpenPopup(assets_popup_name);
+
+			if (ImGui::BeginPopup(assets_popup_name))
+			{
+				if (ImGui::Selectable("Import Asset"))
+				{
+					m_Manager->m_ImportAssetWindow.m_Active = true;
+				}
+
+				if (ImGui::Selectable("View Assets List"))
+				{
+					// Action.
+				}
+
+				ImGui::EndPopup();
+			}
+		}
+		
+		{
+			constexpr auto cheats_popup_name = "CheatsPopup";
+
+			ImGui::SameLine();
+			if (ImGui::Button("Cheats", styles.toolbar_button_size))
+				ImGui::OpenPopup(cheats_popup_name);
+
+			if (ImGui::BeginPopup(cheats_popup_name))
+				ImGui::EndPopup();
+		}
+
+		{
+			const char* npc_popup_name = "NPCPopup";
+
+			ImGui::SameLine();
+			if (ImGui::Button("NPC", styles.toolbar_button_size))
+				ImGui::OpenPopup(npc_popup_name);
+
+			if (ImGui::BeginPopup(npc_popup_name))
+				ImGui::EndPopup();
+		}
+		
+		{
+			constexpr auto level_popup_name = "LevelPopup";
+
+			ImGui::SameLine();
+			if (ImGui::Button("Level", styles.toolbar_button_size))
+				ImGui::OpenPopup(level_popup_name);
+
+			if (ImGui::BeginPopup(level_popup_name))
+				ImGui::EndPopup();
+		}
+
+		styles.toolbar_pop_combobox_style();
+		styles.toolbar_pop_button_style();
+
+	}
+	ImGui::End();
+}
+
+/////////////////////////////////////////////////////////
+/* ImGuiImportAssetWindow */
+void ImGuiImportAssetWindow::Update(float _dt)
 {
 
 }
 
-void ImGuiMenubar::Render()
+void ImGuiImportAssetWindow::Render()
 {
+	if (!m_Active)
+		return;
 
+	if (ImGui::Begin("Import Asset Window", &m_Active))
+	{
+		constexpr auto dialog_name = "ImportAssetFileDialog";
+		if (ImGui::Button("Select file"))
+		{
+			ImGuiFileDialog::Instance()->OpenDialog(dialog_name, "Choose File", ".cpp,.h,.hpp", ".");
+		}
+
+		if (ImGuiFileDialog::Instance()->Display(dialog_name))
+		{
+			if (ImGuiFileDialog::Instance()->IsOk())
+			{
+				const std::string filePathName = ImGuiFileDialog::Instance()->GetFilePathName();
+				const std::string filePath = ImGuiFileDialog::Instance()->GetCurrentPath();
+
+				std::cout << "[ImGuiImportAssetWindow] File path name: " << filePathName << "\n";
+				std::cout << "[ImGuiImportAssetWindow] File path: " << filePath << "\n";
+			}
+
+			ImGuiFileDialog::Instance()->Close();
+		}
+	}
+
+	ImGui::End();
 }
 
 ////////////////////////////////////////////////////////
 /* ImGuiManager */
+ImGuiManager::ImGuiManager()
+	: m_Toolbar(this)
+{
+
+}
+
 void ImGuiManager::ToggleActiveState()
 {
-	m_Active = !m_Active;
+	m_Active					 = !m_Active;
+	m_Toolbar.m_Active = !m_Toolbar.m_Active;
 }
 
 void ImGuiManager::Update(float _dt)
 {
 	if (!m_Active)
 		return;
+
+	m_Toolbar.Update(_dt);
+	m_ImportAssetWindow.Update(_dt);
 }
 
 void ImGuiManager::Render()
@@ -35,9 +165,6 @@ void ImGuiManager::Render()
 	if (!m_Active)
 		return;
 	
-	{
-		ImGui::Begin("Hello, world!");
-		ImGui::Button("Some debug button.");
-		ImGui::End();
-	}
+	m_Toolbar.Render();
+	m_ImportAssetWindow.Render();
 }
--- engine/Tools/ImGuiTools.hpp
+++ engine/Tools/ImGuiTools.hpp
@@ -1,19 +1,105 @@
 #pragma once
 
+#include <imgui/imgui.h>
+
 namespace Tools
 {
+	constexpr bool INIT_ACTIVE_STATE = false;
+
 	/////////////////////////////////////////////////////////
-	class IImGuiWidget
+	namespace utils
 	{
-		virtual void Update(float _dt) = 0;
-		virtual void Render() = 0;
+		/////////////////////////////////////////////////////////
+		struct flags
+		{
+			int toolbar_window_flags = ImGuiWindowFlags_NoResize |
+				ImGuiWindowFlags_NoMove |
+				ImGuiWindowFlags_NoCollapse |
+				ImGuiWindowFlags_NoTitleBar |
+				ImGuiWindowFlags_NoScrollbar |
+				ImGuiWindowFlags_NoScrollWithMouse |
+				ImGuiWindowFlags_NoBringToFrontOnFocus |
+				ImGuiWindowFlags_NoSavedSettings |
+				ImGuiWindowFlags_MenuBar;
+		};
+
+		struct styles
+		{
+			void toolbar_push_combobox_style()
+			{
+				ImGui::PushStyleColor(ImGuiCol_Header, ImVec4(0.26f, 0.59f, 0.98f, 0.31f));
+				ImGui::PushStyleColor(ImGuiCol_HeaderHovered, ImVec4(0.26f, 0.59f, 0.98f, 0.80f));
+				ImGui::PushStyleColor(ImGuiCol_HeaderActive, ImVec4(0.26f, 0.59f, 0.98f, 1.00f));
+				ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(0.16f, 0.29f, 0.48f, 0.54f));
+				ImGui::PushStyleColor(ImGuiCol_FrameBgHovered, ImVec4(0.26f, 0.59f, 0.98f, 0.40f));
+				ImGui::PushStyleColor(ImGuiCol_FrameBgActive, ImVec4(0.26f, 0.59f, 0.98f, 0.67f));
+				ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.93f, 0.94f, 0.95f, 0.96f));
+			}
+
+			void toolbar_push_button_style()
+			{
+				ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.2f, 0.2f, 0.2f, 1.f));
+				ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.3f, 0.3f, 0.3f, 1.f));
+				ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.4f, 0.4f, 0.4f, 1.f));
+			}
+
+			void toolbar_pop_combobox_style()
+			{
+				ImGui::PopStyleColor(7);
+			}
+
+			void toolbar_pop_button_style()
+			{
+				ImGui::PopStyleColor(3);
+			}
+
+			ImVec2 toolbar_button_size = ImVec2(60.f, 20.f);
+		};
 	};
 
 	/////////////////////////////////////////////////////////
-	class ImGuiMenubar : public IImGuiWidget
+	class IImGuiWidget
 	{
-		void Update(float _dt) override;
-		void Render() override;
+		public:
+
+			IImGuiWidget() = default;
+			virtual ~IImGuiWidget() = default;
+
+			virtual void Update(float _dt) = 0;
+			virtual void Render() = 0;
+
+		public:
+
+			bool m_Active = INIT_ACTIVE_STATE;
+	};
+
+	/////////////////////////////////////////////////////////
+
+	class ImGuiManager;
+	class ImGuiToolbar : public IImGuiWidget
+	{
+		public:
+
+			ImGuiToolbar(ImGuiManager* _mgr);
+			~ImGuiToolbar() = default;
+
+			void Update(float _dt) override;
+			void Render() override;
+
+		private:
+
+			ImGuiManager* m_Manager = nullptr;
+	};
+	/////////////////////////////////////////////////////////
+	class ImGuiImportAssetWindow : public IImGuiWidget
+	{
+		public:
+
+			ImGuiImportAssetWindow() = default;
+			~ImGuiImportAssetWindow() = default;
+
+			void Update(float _dt) override;
+			void Render() override;
 	};
 
 	/////////////////////////////////////////////////////////
@@ -21,7 +107,7 @@ namespace Tools
 	{
 		public:
 
-			ImGuiManager() = default;
+			ImGuiManager();
 			~ImGuiManager() = default;
 
 			void ToggleActiveState();
@@ -29,8 +115,14 @@ namespace Tools
 			void Update(float _dt);
 			void Render();
 
+		public:
+
+			ImGuiImportAssetWindow m_ImportAssetWindow;
+
 		private:
 
-			bool m_Active : 1 = false;
+			ImGuiToolbar m_Toolbar;
+
+			bool m_Active = INIT_ACTIVE_STATE;
 	};
 };
